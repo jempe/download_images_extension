0d99ef4e6a5a1c9a136c5387214a3a987a264082
Jempe <info@jempe.org>
2024-01-23 17:17:25 -0500

    add md file

--- content.js
+++ content.js
@@ -1,12 +1,16 @@
 (function() {
 	const login_button = document.querySelector("nav a[href^='/login']");
 
+	// only display button when using public link and not logged in
 	if(login_button) {
+		// check all the steps
 		if(document.querySelectorAll('.step').length > 0) {
+			// add download images button
 			const download_button = document.createElement('button');
 			download_button.innerText = 'Download Images';
-			download_button.addEventListener('click', get_images);
 			download_button.setAttribute('style', 'background: #ff0081; color: white; padding: 0 20px; border-radius: 5px; margin-left: 15px;');
+
+			download_button.addEventListener('click', get_images);
 
 			login_button.closest("div").appendChild(download_button);
 
@@ -20,24 +24,55 @@
 	}
 
 	function get_images() {
+
+		// get all step images
 		const images = document.querySelectorAll('.step img');
 		if (images && images.length > 0) {
 			log('Found ' + images.length + ' images on this page.');
 			var urls = [];
 
-			const prefix = snake_case(document.querySelector('h1').innerText);
+			let content = "";
+
+			const title = document.querySelector('h1').innerText;
+
+			content += "## " + title + "\n\n";
+
+			const prefix = snake_case(title);
 
 			for (var i = 0; i < images.length; i++) {
 
 				const number_with_leading_zero = ("0" + (i + 1)).slice(-2);
 
-				const suffix = snake_case(images[i].closest('.step').querySelector('h3 > span.text-xl > span').innerText);
+				const step_title = images[i].closest('.step').querySelector('h3 > span.text-xl > span').innerText;
+
+				const step_description = images[i].closest('.step').querySelector('p').innerText;
+
+				const suffix = snake_case(step_title);
+
+				// create a filename for the image
+				const file_name = prefix + number_with_leading_zero + "_" + suffix + ".webp";
+
+				content +=  (i + 1)  + ". " + step_description + "\n\n";
+
+				content += "![" + step_title + "](https://raw.githubusercontent.com/wiki/github_user/wiki_name/wiki_images_path/" + file_name + ")\n\n";
 
 				urls.push({
 					src: images[i].src,
-					filename: prefix + number_with_leading_zero + "_" + suffix + ".webp"
+					filename: file_name
 				});
 			}
+
+			//create a base64 encoded string from the content
+			const base64 = btoa(content);
+
+			//create a data url from the base64 encoded string
+			const dataUrl = "data:text/plain;base64," + base64;
+
+			//add the markdown file to the urls array
+			urls.push({
+				src: dataUrl,
+				filename: prefix + ".md"
+			});
 
 			chrome.runtime.sendMessage({
 				action: 'download',
